294f0b0113ca49b396fe36010f0b97cfefb5f512
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-03 15:09:57 -0600

    Assert Merchant data structure'

--- spec/requests/ap1/v1/merchants_request_spec.rb
+++ spec/requests/ap1/v1/merchants_request_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 describe "Rail Engine API" do
-    it "sends a list of merchants" do
-      create_list(:merchant, 50)
+  it "sends a list of merchants" do
+    create_list(:merchant, 50)
 
-      get '/api/v1/merchants'
+    get '/api/v1/merchants'
 
-      expect(response).to be_successful
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
     end
+  end
 end
