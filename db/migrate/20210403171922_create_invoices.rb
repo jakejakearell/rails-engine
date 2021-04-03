b69991823ede4b6c56963643f1e87aced9209655
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-03 11:35:14 -0600

    Fix _id errors in migration

--- db/migrate/20210403171922_create_invoices.rb
+++ db/migrate/20210403171922_create_invoices.rb
@@ -1,10 +1,10 @@
 class CreateInvoices < ActiveRecord::Migration[5.2]
   def change
     create_table :invoices do |t|
-      t.references :customer_id, foreign_key: true
-      t.references :merchant_id, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.references :merchant, foreign_key: true
       t.string :status
-      
+
       t.timestamps
     end
   end
