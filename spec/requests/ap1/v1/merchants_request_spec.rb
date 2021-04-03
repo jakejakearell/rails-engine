87d85bcf06d7aa6508c1d52ead0657a3c071f300
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-03 15:18:32 -0600

    Add basic assertions for merchant end point

--- spec/requests/ap1/v1/merchants_request_spec.rb
+++ spec/requests/ap1/v1/merchants_request_spec.rb
@@ -1,18 +1,36 @@
 require 'rails_helper'
 
-describe "Rail Engine API" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 50)
+describe "Rail Engine API-Merchants" do
+  describe 'Happy Paths' do 
+    it "sends a list of merchants" do
+      create_list(:merchant, 50)
 
-    get '/api/v1/merchants'
+      get '/api/v1/merchants'
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      merchants = JSON.parse(response.body, symbolize_names: true)
 
-    merchants.each do |merchant|
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(Integer)
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+      end
+    end
+
+    it "sends a merchant" do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_a(Integer)
+      expect(merchant[:id]).to eq(id)
 
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
