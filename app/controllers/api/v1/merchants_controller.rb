088e27b892c41ec45045a764dd2cae36611c0aae
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-03 18:42:38 -0600

    Add code for Merchant Happy path

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::MerchantsController < ApplicationController
+  # before_action :page_count
+
   def index
-    render json: Merchant.all
+    render json: Merchant.all.limit(query_size).offset((query_size.to_i * page_count.to_i))
   end
 
   def show
