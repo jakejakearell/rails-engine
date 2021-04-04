eac2ebb9e7a5ea2c17c0c7fdb82545db50e73215
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-04 12:25:18 -0600

    Add assertion for merchant happy path

--- spec/requests/ap1/v1/merchant_request_spec.rb
+++ spec/requests/ap1/v1/merchant_request_spec.rb
@@ -10,12 +10,32 @@ describe "Rail Engine API-Merchant" do
       merchant = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
+      expect(merchant).to be_a(Hash)
+      expect(merchant).to have_key(:data)
+      expect(merchant[:data]).to be_a(Hash)
 
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to eq(id)
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_a(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:data]).to have_key(:type)
+      expect(merchant[:data][:type]).to eq('merchant')
+
+      expect(merchant[:data]).to have_key(:attributes)
+      expect(merchant[:data][:attributes]).to be_a(Hash)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+    end
+  end
+
+  describe 'Sad Paths' do
+    xit "sends a 404 when an invalid id is sent" do
+      id = 12399123
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
     end
   end
 end
