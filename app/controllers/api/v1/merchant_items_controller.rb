5a478849b7548228200f19ce7ad2da62c68b3e34
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-05 17:45:17 -0600

    Collect merchant's items

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,8 +2,13 @@ class Api::V1::MerchantItemsController < ApplicationController
   # before_action :page_count
 
   def index
-    render json: MerchantSerializer.new(
-      Merchant.find(params[:id]).items
-    )
+    merchant_items = render json:MerchantSerializer.new(
+                Merchant.find(params[:id])
+                )
+    merchant_items = JSON.parse(merchant_items)
+    merchant_items = merchant_items["data"]["relationships"]["items"]["data"]
+    hope_you_work = merchant_items.reduce([]) do |memo, item|
+      memo << ItemSerializer.new(Item.find(item["id"]))
+    end
   end
 end
