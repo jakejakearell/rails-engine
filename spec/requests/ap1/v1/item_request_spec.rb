d83d3e7bda3deea3130498b8a0f56d63b74eff56
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-05 18:37:03 -0600

    Add assertions for Item endpoint

---
+++ spec/requests/ap1/v1/item_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe "Rail Engine API-Item" do
+  describe 'Happy Paths' do
+    it "sends a item" do
+      id = create(:item).id
+
+      get "/api/v1/items/#{id}"
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(item).to be_a(Hash)
+      expect(item).to have_key(:data)
+      expect(item[:data]).to be_a(Hash)
+
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_a(String)
+
+      expect(item[:data]).to have_key(:type)
+      expect(item[:data][:type]).to eq('item')
+
+      expect(item[:data]).to have_key(:attributes)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+      expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+    end
+
+    xit "sends all items that belong to a item" do
+
+    end
+  end
+
+  describe 'Sad Paths' do
+    xit "sends a 404 when an invalid id is sent" do
+    end
+  end
+end
