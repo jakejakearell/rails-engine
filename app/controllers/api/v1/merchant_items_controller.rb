3d77da8ebb2bd1bcf1205e2cb13bc3a79cd4669e
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-06 12:13:04 -0600

    Remove previous method/leverage AR records

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,13 +2,7 @@ class Api::V1::MerchantItemsController < ApplicationController
   # before_action :page_count
 
   def index
-    merchant_items = render json:MerchantSerializer.new(
-                Merchant.find(params[:id])
-                )
-    merchant_items = JSON.parse(merchant_items)
-    merchant_items = merchant_items["data"]["relationships"]["items"]["data"]
-    hope_you_work = merchant_items.reduce([]) do |memo, item|
-      memo << ItemSerializer.new(Item.find(item["id"]))
-    end
+    # I would like to abstract out the Merchant items method a bit
+    render json: ItemSerializer.new(Merchant.find(params[:id]).items)
   end
 end
