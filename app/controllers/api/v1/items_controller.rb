28a0c41fabe9d282d18896d4dc0f7f2cab604da0
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-05 18:22:36 -0600

    Move Offset to Application Controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::ItemsController < ApplicationController
   # before_action :page_count
 
   def index
+    render json: ItemSerializer.new(Item.all
+    .limit(query_size)
+    .offset(page_offset)
+    )
   end
 
   def show
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::API
   def page_count
     @count = params[:page] || 0
   end
+
+
+  def page_offset
+    if page_count.to_i == 0
+      0
+    else
+      (query_size.to_i) * (page_count.to_i - 1)
+    end
+  end
 end
