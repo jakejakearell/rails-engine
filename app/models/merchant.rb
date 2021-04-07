28ec9b3d7a946c12798bcf66ad592664652ace40
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-07 13:38:43 -0600

    Add total_revenue_method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,14 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def self.total_revenue(merchant_id)
+    revenue = Merchant.find(merchant_id)
+    .transactions
+    .select("transactions.result, invoices.status, invoice_items.quantity, invoice_items.unit_price")
+    .where("transactions.result = ?", "success")
+    .where("invoices.status = ?", "shipped")
+    .sum("(invoice_items.quantity * invoice_items.unit_price)")
+    return {:id => merchant_id, :merchant_revenue => revenue}
+  end
 end
