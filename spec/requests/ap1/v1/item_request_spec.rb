0bb56ef766999753ebe74d8ad61373aab82f06fa
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-06 18:22:01 -0600

    Add assertions for updating

--- spec/requests/ap1/v1/item_request_spec.rb
+++ spec/requests/ap1/v1/item_request_spec.rb
@@ -56,7 +56,17 @@ describe "Rail Engine API-Item" do
     end
 
     it "can update an item" do
+      id = create(:item).id
+      previous_name = Item.last.name
+      item_params = { name: "The Big Dog" }
+      headers = {"CONTENT_TYPE" => "application/json"}
 
+      patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+      item = Item.find_by(id: id)
+
+      expect(response).to be_successful
+      expect(item.name).to_not eq(previous_name)
+      expect(item.name).to eq("The Big Dog")
     end
 
     xit "can destroy an item" do
