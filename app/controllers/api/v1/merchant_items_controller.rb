268e886202a2fff304ebe3dbe156f8dddaa8ec2d
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-06 20:06:36 -0600

    Remove commented out line

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::MerchantItemsController < ApplicationController
-  # before_action :page_count
-
+  
   def index
     # I would like to abstract out the Merchant items method a bit
     render json: ItemSerializer.new(Merchant.find(params[:id]).items)
