a2b2435d6cc2cf3cae81e9be1610f45b9ec57e4d
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-07 13:25:53 -0600

    Add strong params for merchant id

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::RevenueController < ApplicationController
   def merchant_revenue
-    
+    begin
+      merchant = Merchant.total_revenue(merchant_id)
+      merchant = RevenueMerchant.new(merchant)
+      render json: MerchantRevenueSerializer.new(merchant)
+    rescue ActiveRecord::RecordNotFound
+      render json: {error: "No such merchant",status: 404}, status: 404
+    end
+  end
+
+  private
+
+  def merchant_id
+    params[:id]
   end
 end
