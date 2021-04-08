79482ff50773009177818ba97a10ea70bcd6d760
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-07 19:59:46 -0600

    Add assertions for UnshippedObject object

---
+++ spec/poros/unshipped_order_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe UnshippedOrder do
+  describe 'happy path' do
+    it 'has attributes of id, merchant_revenue and is a UnshippedOrder object' do
+      data = [44 , {:id=>44,
+              :potential_revenue=> 123.99,
+              }]
+
+      result = UnshippedOrder.new(data)
+      expect(result).to be_a(UnshippedOrder)
+      expect(result.id).to eq(44)
+      expect(result.potential_revenue).to eq(123.99)
+    end
+  end
+end
