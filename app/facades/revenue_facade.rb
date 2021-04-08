6a7e1850c633eabd3277269684861e6239da798c
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-08 17:10:10 -0600

    Add new methods to facade

--- app/facades/revenue_facade.rb
+++ app/facades/revenue_facade.rb
@@ -1,4 +1,5 @@
 class RevenueFacade
+  
   def self.potential_revenue_list(quantity)
     Invoice.potential_revenue(quantity)
   end
@@ -16,6 +17,29 @@ class RevenueFacade
     data = RevenueFacade.data_about_invoices(quantity)
     data.map do |data|
       UnshippedOrder.new(data)
+    end
+  end
+
+  def self.sanatize_active_record_data(limit)
+    Merchant.most_revenue
+    .sort_by(&:last)
+    .reverse[0..(limit-1)]
+  end
+
+  def self.top_merchants_poro_data(limit)
+    merchant_info = RevenueFacade.sanatize_active_record_data(limit)
+    merchant_info.reduce(Hash.new) do |memo, merchant|
+      memo[merchant[0]] = {merchant_id: merchant[0],
+                           name: Merchant.merchant_name(merchant[0]),
+                           revenue: merchant[1]}
+      memo
+    end
+  end
+
+  def self.top_merchants_revenue(quantity)
+    data = RevenueFacade.top_merchants_poro_data(quantity)
+    data.map do |data|
+      TopRevenue.new(data)
     end
   end
 end
