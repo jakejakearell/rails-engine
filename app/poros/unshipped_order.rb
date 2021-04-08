5e8c6aab62aa5c2dcc0a0b0015fe739d7a4ae483
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-07 20:00:07 -0600

    Create Unshipped Order Poro

---
+++ app/poros/unshipped_order.rb
@@ -0,0 +1,10 @@
+class UnshippedOrder
+  attr_reader :id,
+              :potential_revenue
+
+  def initialize(data)
+    @id = data.last[:id]
+    @potential_revenue = data.last[:potential_revenue]
+  end
+
+end
