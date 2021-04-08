30c8fc6e0845d1bd9abba353aee7ad7f9a6085e3
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-07 19:49:20 -0600

    Add to factories

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :invoice_item do
-    
+    item
+    invoice
+    quantity { 12}
+    unit_price { 10.00}
   end
 end
--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :invoice do
-    
+    status {'packaged'}
+    merchant_id {10}
+    customer_id {10}
   end
 end
