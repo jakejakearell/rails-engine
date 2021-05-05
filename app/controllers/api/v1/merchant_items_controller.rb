d6a6c537a1a5de3bf6850d5538c5624353e7b18d
Jacob Arellano <jmm.arellano@gmail.com>
2021-05-05 14:37:53 -0600

    Formatting changes

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MerchantItemsController < ApplicationController
 
   def index
     begin
-      # I would like to abstract out the Merchant items method a bit
       render json: ItemSerializer.new(Merchant.find(params[:id]).items)
     rescue ActiveRecord::RecordNotFound
       render json: {error: "No such merchant",status: 404}, status: 404
